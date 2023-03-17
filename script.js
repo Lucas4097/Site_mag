f00fc16b7b8d38ef713bc5f344b022756f46c46b
Lucas <luquinhassantoslucas11@outlook.com>
2023-03-17 18:28:27 -0300

    responsividade

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+// navbar
+let navbar = document.querySelector('.navbar');
+let button = document.querySelector('.menu-btn');
+
+button.addEventListener('click', function(){
+    navbar.classList.toggle('navbar-active');
+    button.classList.toggle('menu-btn-active');
+})
+
+// carousel
 let count = 1;
 document.getElementById("radio1").checked = true;
 
